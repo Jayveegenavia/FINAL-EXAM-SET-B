ab8446d64b38495349c8a7a17c2c552ca87fde94
GERSOME GARCERA <99703106+Gersam4@users.noreply.github.com>
2023-01-13 19:26:45 +0700

    init

---
+++ exam.js
@@ -0,0 +1,17 @@
+const add = require('./add')
+const read = require('./read')
+
+const cmd = process.argv
+var note = {}
+if(cmd[2]){
+    note = {
+        employee: cmd [2],
+        role: cmd [3],
+        department: cmd [4],
+    }
+    var oldnote = read()
+    add(note, oldnote)
+}
+if (cmd[2]== 'read'){
+    present(read())
+}
